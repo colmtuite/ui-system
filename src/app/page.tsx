ab28ea32e9e5ab619b1c4ab70ba5029c1cf45a8e
colmtuite <colmtuite@gmail.com>
2023-08-31 19:28:20 +0100

    Clean up demo page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,105 +1,123 @@
-import { Badge, Button, Status, Text } from "../components";
+import { Badge, Button, Flex, Status, Text } from "../components";
 
 export default function Home() {
   return (
     <main>
-      <Badge>Size 1</Badge>
-      <Badge size="2" mr="3">
-        Size 2
-      </Badge>
-      <Badge size="2">Gray</Badge>
-      <Badge size="2" color="purple">
-        Purple
-      </Badge>
-      <Badge size="2" color="blue">
-        Blue
-      </Badge>
-      <Badge size="2" color="green">
-        Hello
-      </Badge>
-      <Badge size="2" color="yellow">
-        Yellow
-      </Badge>
-      <Badge size="2" color="red">
-        Red
-      </Badge>
-      <Badge size="2" color="orange">
-        Orange
-      </Badge>
-      <Badge size="2" color="bronze">
-        Bronze
-      </Badge>
-      <Badge size="2" color="gold">
-        Gold
-      </Badge>
-      <Text>Fuck me pink</Text>
-      <Text size="9">Fuck me pink</Text>
-      <Text size="8">Fuck me pink</Text>
-      <Text size="7">Fuck me pink</Text>
-      <Text size="6">Fuck me pink</Text>
-      <Text size="5">Fuck me pink</Text>
-      <Text size="4">Fuck me pink</Text>
-      <Text size="3">Fuck me pink</Text>
-      <Text size="2">Fuck me pink</Text>
-      <Text size="1">Fuck me pink</Text>
+      <Flex>
+        <Badge>Size 1</Badge>
+        <Badge size="2" mr="3">
+          Size 2
+        </Badge>
+      </Flex>
+      <Flex gap="5">
+        <Badge size="2">Gray</Badge>
+        <Badge size="2" color="purple">
+          Purple
+        </Badge>
+        <Badge size="2" color="blue">
+          Blue
+        </Badge>
+        <Badge size="2" color="green">
+          Hello
+        </Badge>
+        <Badge size="2" color="yellow">
+          Yellow
+        </Badge>
+        <Badge size="2" color="red">
+          Red
+        </Badge>
+        <Badge size="2" color="orange">
+          Orange
+        </Badge>
+        <Badge size="2" color="bronze">
+          Bronze
+        </Badge>
+        <Badge size="2" color="gold">
+          Gold
+        </Badge>
+      </Flex>
 
-      <Text size="7" color="gray" weight="1">
-        Fuck me pink
-      </Text>
-      <Text size="7" color="blue" weight="1">
-        Fuck me pink
-      </Text>
-      <Text size="7" color="green" weight="1">
-        Fuck me pink
-      </Text>
-      <Text size="7" color="yellow" weight="1">
-        Fuck me pink
-      </Text>
-      <Text size="7" color="red" weight="1" align="center">
-        Fuck me pink
-      </Text>
+      <Flex direction="column" gap="4">
+        <Text size="9">Fuck me pink</Text>
+        <Text size="8">Fuck me pink</Text>
+        <Text size="7">Fuck me pink</Text>
+        <Text size="6">Fuck me pink</Text>
+        <Text size="5">Fuck me pink</Text>
+        <Text size="4">Fuck me pink</Text>
+        <Text size="3">Fuck me pink</Text>
+        <Text size="2">Fuck me pink</Text>
+        <Text size="1">Fuck me pink</Text>
+      </Flex>
 
-      <Button size="1">Create Field</Button>
-      <Button size="2">Create Field</Button>
-      <Button size="3" color="blue">
-        Request a Demo
-      </Button>
-      <Button size="2" color="gray">
-        Create Field
-      </Button>
-      <Button size="2" color="blue">
-        Create Field
-      </Button>
-      <Button size="2" color="green">
-        Create Field
-      </Button>
-      <Button size="2" color="red">
-        Create Field
-      </Button>
-      <Button size="2" color="gray" ghost>
-        Create Field
-      </Button>
-      <Button size="2" color="blue" ghost>
-        Create Field
-      </Button>
-      <Button size="2" color="green" ghost>
-        Create Field
-      </Button>
-      <Button size="2" color="red" ghost>
-        Create Field
-      </Button>
+      <Flex direction="column" gap="4">
+        <Text size="7" color="gray" weight="1">
+          Fuck me pink
+        </Text>
+        <Text size="7" color="blue" weight="1">
+          Fuck me pink
+        </Text>
+        <Text size="7" color="green" weight="1">
+          Fuck me pink
+        </Text>
+        <Text size="7" color="yellow" weight="1">
+          Fuck me pink
+        </Text>
+        <Text size="7" color="red" weight="1" align="center">
+          Fuck me pink
+        </Text>
+      </Flex>
 
-      <Status size="1" />
-      <Status size="2" />
-      <Status size="2" color="gray" />
-      <Status size="2" color="green" />
-      <Status size="2" color="yellow" />
-      <Status size="2" color="red" />
+      <Flex gap="4">
+        <Button size="1">Create Field</Button>
+        <Button size="2">Create Field</Button>
+        <Button size="3" color="blue">
+          Request a Demo
+        </Button>
+      </Flex>
+
+      <Flex gap="4">
+        <Button size="2" color="gray">
+          Create Field
+        </Button>
+        <Button size="2" color="blue">
+          Create Field
+        </Button>
+        <Button size="2" color="green">
+          Create Field
+        </Button>
+        <Button size="2" color="red">
+          Create Field
+        </Button>
+      </Flex>
+
+      <Flex gap="4">
+        <Button size="2" color="gray" ghost>
+          Create Field
+        </Button>
+        <Button size="2" color="blue" ghost>
+          Create Field
+        </Button>
+        <Button size="2" color="green" ghost>
+          Create Field
+        </Button>
+        <Button size="2" color="red" ghost>
+          Create Field
+        </Button>
+      </Flex>
+
+      <Flex gap="4">
+        <Status size="1" />
+        <Status size="2" />
+        <Status size="2" color="gray" />
+        <Status size="2" color="green" />
+        <Status size="2" color="yellow" />
+        <Status size="2" color="red" />
+      </Flex>
 
       <div
         style={{
-          width: 300,
-          height: 120,
+          width: 250,
+          height: 80,
           borderRadius: 6,
           borderWidth: 1,
           borderStyle: "solid",
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export * from './badge';
 export * from './button';
-export * from './text';
+export * from './flex';
 export * from './status';
+export * from './text';
